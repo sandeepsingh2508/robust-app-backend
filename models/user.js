2f31c9b337aac559efadc516b21e745496524254
sandeepsingh2508 <prasan.250899@gmail.com>
2023-12-30 17:20:17 +0530

    schema validation added

--- models/user.js
+++ models/user.js
@@ -1,22 +1,34 @@
 const mongoose = require("mongoose");
+const validator=require("validator")
 
 const userSchema = mongoose.Schema(
   {
     name: {
       type: String,
-      requied: true,
+      required: [true, "Please enter your name"],
+      maxLength: [30, "Name cannot exceed 30 charecter"],
+      minLength: [4, "Name should have more than 4 charecter"],
     },
     email: {
       type: String,
-      requied: true,
+      required: [true, "please enter your email"],
+      unique: true,
+      validate: [validator.isEmail, "please enter valid email"],
     },
     password: {
       type: String,
-      requied: true,
+      required: [true, "please enter your password"],
+      minLength: [5, "password should be greater than 5 charecter"],
     },
     phone: {
       type: String,
-      requied: true,
+      required: true,
+      validate: {
+        validator: function (value) {
+          return value.length === 10;
+        },
+        message: "Phone number must be 10 digits long!",
+      },
     },
     role: {
       type: String,
